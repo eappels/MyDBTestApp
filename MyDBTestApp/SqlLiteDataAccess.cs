d204daf12283e3baced8014952c72d0e9b246cc6
eappels <appels@telenet.be>
2020-05-19 20:14:21 +0200

    Revert "Encryption"
    This reverts commit e75fb6bd1ddcff34d4d78d078272d899183e7728.

--- MyDBTestApp/SqlLiteDataAccess.cs
+++ MyDBTestApp/SqlLiteDataAccess.cs
@@ -4,46 +4,25 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using Dapper;
-using System.Security.Cryptography;
-using System.Text;
-using System.IO;
-using System;
 
 namespace MyDBTestApp
 {
     public class SqlLiteDataAccess
     {
-
-        private static string EncryptionKey = "b14ca5898a4e4133bbce2ea2315a1916";
-
         public static List<PersonModel> LoadAllPeople()
         {
             using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
             {
                 var output = conn.Query<PersonModel>("Select * from Persons", new DynamicParameters());
-                List<PersonModel> persons = output.ToList();
-                foreach (PersonModel model in persons)
-                {
-                    PersonModel decryptedPerson = new PersonModel() {
-                        Id = model.Id,
-                        Firstname = DecryptString(EncryptionKey, model.Firstname),
-                        LastName = DecryptString(EncryptionKey, model.LastName)
-                    };
-                }
                 return output.ToList();
             }
         }
 
         public static void SavePerson(PersonModel person)
         {
-            PersonModel encryptedPerson = new PersonModel() {
-                Id = person.Id,
-                Firstname = EncryptString(EncryptionKey, person.Firstname),
-                LastName = EncryptString(EncryptionKey, person.LastName)
-            };
             using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
             {
-                conn.Execute("Insert into Persons (FirstName, LastName) values (@FirstName, @LastName)", encryptedPerson);
+                conn.Execute("Insert into Persons (FirstName, LastName) values (@FirstName, @LastName)", person);
             }
         }
 
@@ -58,59 +37,6 @@ namespace MyDBTestApp
         private static string LoadConnectionString(string id = "Default")
         {
             return ConfigurationManager.ConnectionStrings[id].ConnectionString;
-        }
-
-        public static string EncryptString(string key, string plainText)
-        {
-            byte[] iv = new byte[16];
-            byte[] array;
-
-            using (Aes aes = Aes.Create())
-            {
-                aes.Key = Encoding.UTF8.GetBytes(key);
-                aes.IV = iv;
-
-                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
-
-                using (MemoryStream memoryStream = new MemoryStream())
-                {
-                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
-                    {
-                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
-                        {
-                            streamWriter.Write(plainText);
-                        }
-
-                        array = memoryStream.ToArray();
-                    }
-                }
-            }
-
-            return Convert.ToBase64String(array);
-        }
-
-        public static string DecryptString(string key, string cipherText)
-        {
-            byte[] iv = new byte[16];
-            byte[] buffer = Convert.FromBase64String(cipherText);
-
-            using (Aes aes = Aes.Create())
-            {
-                aes.Key = Encoding.UTF8.GetBytes(key);
-                aes.IV = iv;
-                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
-
-                using (MemoryStream memoryStream = new MemoryStream(buffer))
-                {
-                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
-                    {
-                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
-                        {
-                            return streamReader.ReadToEnd();
-                        }
-                    }
-                }
-            }
         }
     }
 }
