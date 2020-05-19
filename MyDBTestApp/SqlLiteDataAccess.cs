0b501068b4a72ccd9f8042aa306e70eede61402c
eappels <appels@telenet.be>
2020-05-19 19:47:38 +0200

    Encryption test

--- MyDBTestApp/SqlLiteDataAccess.cs
+++ MyDBTestApp/SqlLiteDataAccess.cs
@@ -4,6 +4,10 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using Dapper;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+using System;
 
 namespace MyDBTestApp
 {
@@ -14,6 +18,14 @@ namespace MyDBTestApp
             using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
             {
                 var output = conn.Query<PersonModel>("Select * from Persons", new DynamicParameters());
+                foreach (PersonModel pm in output)
+                {
+                    PersonModel p2 = new PersonModel()
+                    {
+                        Firstname = DecryptString("B8 + iXv5 / 8BUQEbHt8 // fGA ==", pm.Firstname),
+                        LastName = DecryptString("B8 + iXv5 / 8BUQEbHt8 // fGA ==", pm.LastName)
+                    };
+                }
                 return output.ToList();
             }
         }
@@ -22,6 +34,10 @@ namespace MyDBTestApp
         {
             using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
             {
+                var encryptedpersonfirstname = EncryptString("", person.Firstname);
+                var encryptedpersonlastname = EncryptString("", person.LastName);
+                person.Firstname = EncryptString("B8 + iXv5 / 8BUQEbHt8 // fGA ==", encryptedpersonfirstname);
+                person.LastName = EncryptString("B8 + iXv5 / 8BUQEbHt8 // fGA ==", encryptedpersonlastname);
                 conn.Execute("Insert into Persons (FirstName, LastName) values (@FirstName, @LastName)", person);
             }
         }
@@ -37,6 +53,59 @@ namespace MyDBTestApp
         private static string LoadConnectionString(string id = "Default")
         {
             return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+
+        public static string EncryptString(string key, string plainText)
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(plainText);
+                        }
+
+                        array = memoryStream.ToArray();
+                    }
+                }
+            }
+
+            return Convert.ToBase64String(array);
+        }
+
+        public static string DecryptString(string key, string cipherText)
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(cipherText);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+                using (MemoryStream memoryStream = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)memoryStream, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader streamReader = new StreamReader((Stream)cryptoStream))
+                        {
+                            return streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
         }
     }
 }
