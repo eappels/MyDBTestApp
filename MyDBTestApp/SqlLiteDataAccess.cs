24ce8c05e8f07e753f8fca2197bdefa482a13989
eappels <appels@telenet.be>
2020-05-20 06:53:28 +0200

    Encryption added

--- MyDBTestApp/SqlLiteDataAccess.cs
+++ MyDBTestApp/SqlLiteDataAccess.cs
@@ -9,20 +9,34 @@ namespace MyDBTestApp
 {
     public class SqlLiteDataAccess
     {
+
         public static List<PersonModel> LoadAllPeople()
         {
             using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
             {
                 var output = conn.Query<PersonModel>("Select * from Persons", new DynamicParameters());
-                return output.ToList();
+                List<PersonModel> decryptedList = new List<PersonModel>();                
+                foreach (PersonModel encryptedPersonModel in output.ToList())
+                {
+                    PersonModel decryptedPersonModel = new PersonModel();
+                    decryptedPersonModel.Id = encryptedPersonModel.Id;
+                    decryptedPersonModel.Firstname = StringEncrypter.Decrypt(encryptedPersonModel.Firstname);
+                    decryptedPersonModel.LastName = StringEncrypter.Decrypt(encryptedPersonModel.LastName);
+                    decryptedList.Add(decryptedPersonModel);
+                }
+                return decryptedList;
             }
         }
 
         public static void SavePerson(PersonModel person)
         {
+            PersonModel encryptedPersonModel = new PersonModel();
+            encryptedPersonModel.Id = person.Id;
+            encryptedPersonModel.Firstname = StringEncrypter.Encrypt(person.Firstname);
+            encryptedPersonModel.LastName = StringEncrypter.Encrypt(person.LastName);
             using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
             {
-                conn.Execute("Insert into Persons (FirstName, LastName) values (@FirstName, @LastName)", person);
+                conn.Execute("Insert into Persons (FirstName, LastName) values (@FirstName, @LastName)", encryptedPersonModel);
             }
         }
 
