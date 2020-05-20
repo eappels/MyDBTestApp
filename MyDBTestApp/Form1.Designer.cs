cedecf95a1b41fd133006f867584841867defdb7
eappels <appels@telenet.be>
2020-05-20 06:23:47 +0200

    Sync before encryption branch

--- MyDBTestApp/Form1.Designer.cs
+++ MyDBTestApp/Form1.Designer.cs
@@ -122,7 +122,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(446, 267);
+            this.ClientSize = new System.Drawing.Size(440, 267);
             this.Controls.Add(this.lst_Output);
             this.Controls.Add(this.btn_Delete);
             this.Controls.Add(this.btn_Refresh);
