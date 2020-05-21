4a925954b62991a3abc14c93367b955e9304a2fb
eappels <appels@telenet.be>
2020-05-21 09:18:14 +0200

    More testing

--- MyDBTestApp.Tests/EncryptionTests.cs
+++ MyDBTestApp.Tests/EncryptionTests.cs
@@ -6,7 +6,21 @@ namespace MyDBTestApp.Tests
     {
 
         [Fact]
-        public void Encrypt_ShouldReturnSameDataAfterEncryptionAndDecrytion()
+        public void Encrypt_ShouldReturnSameDataAfterEncryptionAndDecrytion1()
+        {
+            string tmp = StringEncrypter.Encrypt("EddyAppels", "aaa");
+            string expected = StringEncrypter.Decrypt(tmp, "aaa");
+
+            string tmp2 = StringEncrypter.Encrypt("EddyAppels", "bbb");
+            string actual = StringEncrypter.Decrypt(tmp2, "bbb");
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("Jan Vandamme", "aaabbb")]
+        [InlineData("Jan Vandamme", "cc")]
+        public void Encrypt_ShouldReturnSameDataAfterEncryptionAndDecrytion(string a, string b)
         {
             string tmp = StringEncrypter.Encrypt("EddyAppels", "aaa");
             string expected = StringEncrypter.Decrypt(tmp, "aaa");
